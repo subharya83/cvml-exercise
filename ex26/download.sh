f9e9591dde13e6e763e905954d4b074df7daa4e5
subharya83 <subh.arya@gmail.com>
2025-03-08 18:00:45 -0500

    ex26: Updated Tracker code

--- ex26/download.sh
+++ ex26/download.sh
@@ -1,4 +1,9 @@
 # Download model configuration and weights
 mkdir -p weights
+
+# CNN based model
 wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg -O weights/yolov3.cfg
 wget https://pjreddie.com/media/files/yolov3.weights -O weights/yolov3.weights
+
+# Transformer based model
+wget https://github.com/onnx/models/raw/main/vision/object_detection_segmentation/detr/model/detr-resnet50.onnx -O weights/detr-resnet50.onnx
