3cc8ce238ee84863e61536a9bf8f4e7e5cfb674c
subharya83 <subh.arya@gmail.com>
2025-03-10 18:07:20 -0400

    ex26: Download script link fixed

--- ex26/download.sh
+++ ex26/download.sh
@@ -6,4 +6,4 @@ wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg -O weights/y
 wget https://pjreddie.com/media/files/yolov3.weights -O weights/yolov3.weights
 
 # Transformer based model
-wget https://github.com/onnx/models/raw/main/vision/object_detection_segmentation/detr/model/detr-resnet50.onnx -O weights/detr-resnet50.onnx
+wget https://huggingface.co/qualcomm/DETR-ResNet50/tree/main/detr-resnet50.onnx -O weights/detr-resnet50.onnx
