e5d233cbb9b8e3c0431463bd547d25ec5f846ac4
subharya83 <subh.arya@gmail.com>
2025-04-23 22:16:49 -0400

    ex27: Fixed HOG implementation

--- ex27/src/hog.cpp
+++ ex27/src/hog.cpp
@@ -1,5 +1,40 @@
 #include "hog.h"
+#include "convolution.h"  // For convolve function
 #include <iostream>
+#include <fstream>
+
+std::vector<float> compute_hog(const Image& img, int cell_size, int bin_count) {
+    Image Ix = convolve(img, {-1, 0, 1}, 3); // Sobel X
+    Image Iy = convolve(img, {-1, 0, 1}, 3); // Sobel Y
+
+    std::vector<float> hog_features;
+    for (int cy = 0; cy < img.height; cy += cell_size) {
+        for (int cx = 0; cx < img.width; cx += cell_size) {
+            std::vector<float> bins(bin_count, 0.0f);
+            for (int y = cy; y < cy + cell_size && y < img.height; ++y) {
+                for (int x = cx; x < cx + cell_size && x < img.width; ++x) {
+                    int idx = y * img.width + x;
+                    float gx = Ix.data[idx], gy = Iy.data[idx];
+                    float mag = std::sqrt(gx * gx + gy * gy);
+                    float angle = std::atan2(gy, gx) * 180 / M_PI;
+                    if (angle < 0) angle += 180;
+                    int bin = static_cast<int>(angle / (180 / bin_count)) % bin_count;
+                    bins[bin] += mag;
+                }
+            }
+            hog_features.insert(hog_features.end(), bins.begin(), bins.end());
+        }
+    }
+    return hog_features;
+}
+
+#ifdef USE_SIFT
+float get_gradient_orientation(const Image& Ix, const Image& Iy, int x, int y) {
+    int idx = y * Ix.width + x;
+    float gx = Ix.data[idx], gy = Iy.data[idx];
+    return std::atan2(gy, gx);
+}
+#endif
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -10,4 +45,5 @@ int main(int argc, char** argv) {
     auto hog = compute_hog(img);
     for (float val : hog) std::cout << val << " ";
     std::cout << "\n";
+    return 0;
 }
